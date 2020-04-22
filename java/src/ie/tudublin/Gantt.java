afab938c55a93beb45024e88141aa4bf7199ea92
C18384776 <C18384776@mytudublin.ie>
2020-04-22 14:16:11 +0100

    Added comments. Fixed mouseclick method where getStart wasn't set up correct. Now mouseDrag is broken. Going to fix.

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -7,46 +7,77 @@ import processing.data.TableRow;
 
 public class Gantt extends PApplet
 {	
+
+	// Declare an arraylist.
 	ArrayList<Task> tsk = new ArrayList<Task>();
 
-
-
-
+	// Settings method.
 	public void settings()
 	{
-		size(800, 600);
-	}
 
+		// The size of the window.
+		size(800, 600);
+
+	}
+	// End settings method.
+
+	// loadTask method.
 	public void loadTasks()
 	{
 
+		// Loads table.
         Table table = loadTable("tasks.csv", "header");
 
+		// Adds each row into the arraylist.
         for(TableRow row:table.rows())
         {   
+
             Task p = new Task(row);
 
-            tsk.add(p);
-        }
-	}
+			tsk.add(p);
+			
+		}	
+		// End for loop.
 
+	}
+	// End loadTask method.
+
+	// printTasks method.
 	public void printTasks()
-	{
+	{	
+		// Displays each row in the terminal.
         for(Task t:tsk)
         {
             System.out.println(t);
-        }
+		}
+		// End for loop
 	}
 
 	public void displayTasks() {
+		// All measurments are done for size(800, 600); but feel free to change the size ; it should work on any screen.
 
+		// The left side padding that the text has.
 		float textBorder = width * 0.05f;	// 40
-		float maxAcross = width * 0.95f;     // 720
-		float colours = 255.0f / 30.0f;
+
+		// Top side padding that text has.
 		float textDown = height * 0.1f;		// 60
+
+		// The maximum distance that the line bars can go to.
+		float maxAcross = width * 0.95f;     // 720
+		
+		// Used for colours. The max colour range divided by the arraylist to achieve different colours.
+		float colours = 255.0f / 30;
+
+		// The maximum distance that the text can travel going down the list.
 		float maxDown = height * 0.8f;      // 480
+
+		// The x co - ord of the numbers.
 		float numberMinDown = height * 0.05f;  // 30
+
+		// The maximum distance that the lines drop.
 		float lineMaxDown = height * 0.9f;
+
+		// The small boxes that measure one bar.
 		float numberDiff = (maxAcross - textBorder * 3.0f) / 30;
 		
 		// Generates list of tasks on screen.
@@ -113,20 +144,26 @@ public class Gantt extends PApplet
 
 			int finalValue;
 
+			int startVal = t.getStart();
+
+			int endVal = t.getEnd();
+
 			if(i == 0)
 			{
 				if(mouseX > textBorder * 3.0f && mouseX < maxAcross &&
 				mouseY > 45   && mouseY < 70)
 				{
 					finalValue = valueClickedOn / 20 + 1; // 20 is 30 / 20 --- will explain better later...
-					if (finalValue < t.getStart())
-					{
-						t.setStart(finalValue);
-					}
-					else
+
+					if (finalValue > t.getEnd())
 					{
 						t.setEnd(finalValue);
-					}				
+					}
+
+					if (finalValue < t.getEnd() && finalValue > t.getStart())
+					{
+						t.setStart(finalValue);
+					}			
 				}
 			}
 			else if ( i == 1 )
@@ -135,13 +172,14 @@ public class Gantt extends PApplet
 				mouseY > 90 && mouseY < 115)
 				{
 					finalValue = valueClickedOn / 20 + 1; // 20 is 30 / 20 --- will explain better later...
-					if (finalValue < t.getStart())
-					{
-						t.setStart(finalValue);
-					}
-					else
+					if (finalValue > t.getEnd())
 					{
 						t.setEnd(finalValue);
+					}
+
+					if (finalValue < t.getEnd() && finalValue > t.getStart())
+					{
+						t.setStart(finalValue);
 					}		
 				}
 			}
@@ -151,14 +189,15 @@ public class Gantt extends PApplet
 				mouseY > (boxyArea*j)  && mouseY < (boxyArea*j) + 25)
 				{
 					finalValue = valueClickedOn / 20 + 1; // 20 is 30 / 20 --- will explain better later...
-					if (finalValue < t.getStart())
-					{
-						t.setStart(finalValue);
-					}
-					else
+					if (finalValue > t.getEnd())
 					{
 						t.setEnd(finalValue);
-					}		
+					}
+
+					if (finalValue < t.getEnd() && finalValue > t.getStart())
+					{
+						t.setStart(finalValue);
+					}	
 				}
 			}
 		}
@@ -188,13 +227,14 @@ public class Gantt extends PApplet
 				mouseY > 45   && mouseY < 70)
 				{
 					finalValue = valueClickedOn / 20 + 1; // 20 is 30 / 20 --- will explain better later...
-					if (finalValue < t.getStart())
-					{
-						t.setStart(finalValue);
-					}
-					else
+					if (finalValue > t.getEnd())
 					{
 						t.setEnd(finalValue);
+					}
+
+					if (finalValue < t.getEnd() && finalValue > t.getStart())
+					{
+						t.setStart(finalValue);
 					}				
 				}
 			}
@@ -204,14 +244,15 @@ public class Gantt extends PApplet
 				mouseY > 90 && mouseY < 115)
 				{
 					finalValue = valueClickedOn / 20 + 1; // 20 is 30 / 20 --- will explain better later...
-					if (finalValue < t.getStart())
-					{
-						t.setStart(finalValue);
-					}
-					else
+					if (finalValue > t.getEnd())
 					{
 						t.setEnd(finalValue);
-					}		
+					}
+
+					if (finalValue < t.getEnd() && finalValue > t.getStart())
+					{
+						t.setStart(finalValue);
+					}	
 				}
 			}
 			else
@@ -220,14 +261,15 @@ public class Gantt extends PApplet
 				mouseY > (boxyArea*j)  && mouseY < (boxyArea*j) + 25)
 				{
 					finalValue = valueClickedOn / 20 + 1; // 20 is 30 / 20 --- will explain better later...
-					if (finalValue < t.getStart())
-					{
-						t.setStart(finalValue);
-					}
-					else
+					if (finalValue > t.getEnd())
 					{
 						t.setEnd(finalValue);
-					}		
+					}
+
+					if (finalValue < t.getEnd() && finalValue > t.getStart())
+					{
+						t.setStart(finalValue);
+					}	
 				}
 			}
 		}
